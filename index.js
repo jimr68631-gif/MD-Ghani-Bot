9e5f1126362ae7523a7cac30cec9c5af983a1c5d
jimr68631-gif <jimr68631@gmail.com>
2026-09-21 23:43:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -107,7 +107,7 @@ const config = {
   channelJid: "120363429085670060@newsletter",
   channelLink: "https://whatsapp.com/channel/0029Vb8vvB1Fcow4AY0NeC1p",
   pairingTimeout: 60000,
-  browser: ["Ubuntu", "Chrome", "20.0.04"],
+  browser: ["Windows", "Chrome", "Chrome 114.0.5735.198"],
   alwaysOnline: true,
   sessionDir: "./sessions",
   timezone: "Asia/Karachi",
